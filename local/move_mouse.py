b72524e14786a064557c541067c27712e8e0c7a3
plmoklbj <rowan.sbur@gmail.com>
2022-09-25 17:25:23 +0100

    Adjust scroll speed based on touch

--- local/move_mouse.py
+++ local/move_mouse.py
@@ -34,11 +34,14 @@ class Cursor:
             mouse.move(self.current_x - x, self.current_y - y)
 
     def scroll(self, data):
-        if data[1] > self.first_y:
-            mouse.wheel(delta = 0.2)
-        if data[1] < self.first_y:
-            mouse.wheel(delta = -0.2)
-        self.first_y = data[1]
+        if self.first_touch:
+            self.y = (data[1] * resolution[1])
+            self.first_touch = False
+        else:
+            new_y = (data[1] * resolution[1])
+            scroll = -((self.y - new_y) * 0.1)
+            mouse.wheel(delta = scroll)
+            self.y = new_y
 
 cursor = Cursor()
 
